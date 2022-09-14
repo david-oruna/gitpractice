f3284acf248310352c6328c5a8ec2b70b7cd8f2c
David <cuentaporsi2@gmail.com>
2022-09-13 21:14:01 -0500

    hi

--- sample.py
+++ sample.py
@@ -1,1 +1,33 @@
-import numpy as np
+# Let's code tetris
+
+# Let's make a <3 printer
+
+
+
+banlist = ["Goat", "Cow", "Horse", "Cat", "Dog", "Bird", "Pig"]
+
+def fun():
+    while True:
+        try:
+            take = input("Enter your name: ")
+            if take.isalpha():
+                
+                if take.lower() == "donkey":
+                    print("XD")
+                elif take.lower() == "human":
+                    print("Hey are u an alien? 👽")
+                else:
+                    print("That's a cool name")
+                    for i in take:
+                        print("<3")
+                print('Finishing .. . . ')                  
+                break
+            else:
+                print("Only letters!")
+                continue   
+        except ValueError as e:
+            print(f"Value error {e}")
+
+        
+            
+fun()

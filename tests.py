317fad1b568ffbca078da170f965ed544e3a4ced
David <cuentaporsi2@gmail.com>
2022-09-14 21:35:00 -0500

    .

--- tests.py
+++ tests.py
@@ -1,3 +1,25 @@
 a = ["apple", "banana"]
 
-print("xd" in a)
+print("c" in a)
+
+class Dog:
+ 
+    # class attribute
+    attr1 = "mammal"
+ 
+    # Instance attribute
+    def __init__(self, name):
+        self.name = name
+ 
+# Driver code
+# Object instantiation
+Rodger = Dog("Rodger")
+Tommy = Dog("Tommy")
+ 
+# Accessing class attributes
+print(f"Rodger is a {Rodger.__class__.attr1}")
+print(f"Tommy is also a {Rodger.__class__.attr1}")
+ 
+# Accessing instance attributes
+print(f"My name is {Rodger.name}")
+print(f"My name is {Tommy.name}")
